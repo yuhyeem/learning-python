181ed0a0d068b3eb37d7ac00621f767d198984b0
Yeem <nayeem90325339@gmail.com>
2021-09-04 14:21:19 -0400

    messing with intro to loops here

---
+++ .vscode/loops942021.py
@@ -0,0 +1,13 @@
+board_games = ["Settlers of Catan", "Carcassone", "Power Grid", "Agricola", "Scrabble"]
+
+sport_games = ["football", "hockey", "baseball", "cricket"]
+#printing every game in the list board games
+for game in board_games:
+  print(game)
+#printing every sport in the list sport games
+for sport in sport_games:
+  print(sport)
+
+basketball_players = ["Lebron", "Westbrook", "I hate using the fucking arrow keys", "John Wall",]
+#attempting to understand and run simple loops in one line for readability purposes
+for players in basketball_players : print(players)
