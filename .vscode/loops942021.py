e8751a5eaa4dac68af0b89e7a17c1e80cbc7a67a
Yeem <nayeem90325339@gmail.com>
2021-09-04 14:49:35 -0400

    added using range to control how print amount

--- .vscode/loops942021.py
+++ .vscode/loops942021.py
@@ -11,3 +11,11 @@ for sport in sport_games:
 basketball_players = ["Lebron", "Westbrook", "I hate using the fucking arrow keys", "John Wall",]
 #attempting to understand and run simple loops in one line for readability purposes
 for players in basketball_players : print(players)
+#Printing promise 5 times here
+promise = "I will finish the python loops module!"
+for temp in range(5):
+  print(promise)
+
+#doing my own version of this, printing 10 times
+randomvariable = "arrow key spite"
+for temp in range(10): print(randomvariable)
