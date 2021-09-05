b1db894178adbd03ad9949eaaa462573a76ea3a1
Yeem <nayeem90325339@gmail.com>
2021-09-04 23:43:50 -0400

    nested loop addition

--- .vscode/loops942021.py
+++ .vscode/loops942021.py
@@ -57,3 +57,14 @@ for dog_breed in dog_breeds_available_for_adoption:
     
     print("They have the dog I want!")
     break
+# nested loop
+sales_data = [[12, 17, 22], [2, 10, 3], [5, 12, 13]]
+
+scoops_sold = 0
+
+for location in sales_data:
+  print(location)
+  for element in location:
+    scoops_sold += element
+    
+print(scoops_sold)
