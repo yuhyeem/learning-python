2415bff9a6292ca97f9297a89d1d6297b2aadac2
Yeem <nayeem90325339@gmail.com>
2021-09-04 20:20:00 -0400

    used a while loop to print statement that selects eache lement from list using index location

--- .vscode/loops942021.py
+++ .vscode/loops942021.py
@@ -1,27 +1,27 @@
 board_games = ["Settlers of Catan", "Carcassone", "Power Grid", "Agricola", "Scrabble"]
 
 sport_games = ["football", "hockey", "baseball", "cricket"]
-#printing every game in the list board games
+# printing every game in the list board games
 for game in board_games:
   print(game)
-#printing every sport in the list sport games
+# printing every sport in the list sport games
 for sport in sport_games:
   print(sport)
 
 basketball_players = ["Lebron", "Westbrook", "I hate using the fucking arrow keys", "John Wall",]
-#attempting to understand and run simple loops in one line for readability purposes
+# attempting to understand and run simple loops in one line for readability purposes
 for players in basketball_players : print(players)
 #Printing promise 5 times here
 promise = "I will finish the python loops module!"
 for temp in range(5):
   print(promise)
 
-#doing my own version of this, printing 10 times
+# doing my own version of this, printing 10 times
 randomvariable = "arrow key spite"
 for temp in range(10): print(randomvariable)
 
 
-#Doing a while loop counting down from 10
+# Doing a while loop counting down from 10
 countdown = 10
 while countdown >= 0: 
   print(countdown);
@@ -38,3 +38,13 @@ while countdown2 <= 10:
     print(countdown2);
     countdown2 +=1
 print("We down and up again")
+
+# Here we mess with a while loop to print a statement that selects each element from the list using their index location 
+python_topics = ["variables", "control flow", "loops", "modules", "classes"]
+
+length = len(python_topics)
+index = 0
+while index < length:
+  print("I am learning about", (python_topics[index]));
+  index +=1 
+
