7ef694ded1897b5cda9200617a25f8ce83b139d0
Yeem <nayeem90325339@gmail.com>
2021-09-08 03:24:00 -0400

    cli commit

--- stringspy3.py
+++ stringspy3.py
@@ -57,3 +57,46 @@ print(poem_title_fixed)
 poem_author_fixed = poem_author.upper()
 print(poem_author)
 print(poem_author_fixed)
+
+#split authors list with .split, then made a for loop that split with a space starting from the end 
+authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
+
+author_names = authors.split(',')
+
+print(author_names)
+
+author_last_names = []
+for name in author_names:
+  author_last_names.append(name.split()[-1])
+  
+print(author_last_names)
+
+# used escape sequence to split by line break here
+spring_storm_text = \
+"""The sky has given over 
+its bitterness. 
+Out of the dark change 
+all day long 
+rain falls and falls 
+as if it would never end. 
+Still the snow keeps 
+its hold on the ground. 
+But water, water 
+from a thousand runnels! 
+It collects swiftly, 
+dappled with black 
+cuts a way for itself 
+through green ice in the gutters. 
+Drop after drop it falls 
+from the withered grass-stems 
+of the overhanging embankment."""
+
+# messing with the .join string method
+spring_storm_lines = spring_storm_text.split("\n")
+print (spring_storm_lines)
+
+winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the disattiring are completed!', 'A liquid moon', 'moves gently among', 'the long branches.', 'Thus having prepared their buds', 'against a sure winter', 'the wise trees', 'stand sleeping in the cold.']
+
+winter_trees_full = "\n".join(winter_trees_lines)
+
+print(winter_trees_full)
