b073af13b5277995720478a57fb6e829fe271e3c
Yeem <nayeem90325339@gmail.com>
2021-09-08 01:30:48 -0400

    committing because forgot

--- stringspy3.py
+++ stringspy3.py
@@ -27,3 +27,22 @@ def password_generator(first_name, last_name):
   return temp
 
 temp_password = password_generator(first_name,last_name)
+
+#forgot to commit, last exercise in multiple section string lesson. generating user and pass using more string slice techniques
+def username_generator(first_name, last_name):
+    if len(first_name) < 3:
+        user_name = first_name
+    else:
+        user_name = first_name[0:3]
+    if len(last_name) < 4:
+        user_name += last_name
+    else:
+        user_name += last_name[0:4]
+    return user_name
+  
+    
+def password_generator(user_name):
+    password = ""
+    for i in range(0, len(user_name)):
+        password += user_name[i-1]
+    return password
