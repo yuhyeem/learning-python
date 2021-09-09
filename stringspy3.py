f36e6370734622d686893b5859b9171ff3b4cd54
Yeem <nayeem90325339@gmail.com>
2021-09-09 03:50:51 -0400

    # used string methods to make a list much more readable, then wrote a for loop with .format to print each item from the new lists made in a print statement

--- stringspy3.py
+++ stringspy3.py
@@ -143,3 +143,37 @@ def poem_description(publishing_date, author, title, original_work):
 
 my_beard_description = poem_description("1974", "Shel Silverstein", "My Beard", "Where the Sidewalk Ends")
 print(my_beard_description)
+
+
+# used string methods to make a list much more readable, then wrote a for loop with .format to print each item from the new lists made in a print statement
+highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
+
+print(highlighted_poems)
+
+highlighted_poems_list = highlighted_poems.split(",")
+
+print(highlighted_poems_list)
+highlighted_poems_stripped = []
+
+for whitespace in highlighted_poems_list:
+  highlighted_poems_stripped.append(whitespace.strip(" "))
+print(highlighted_poems_stripped)
+
+highlighted_poems_details = []
+
+for colon in highlighted_poems_stripped:
+  highlighted_poems_details.append(colon.split(":"))
+
+print (highlighted_poems_details)
+
+titles = []
+poets = []
+dates = []
+
+for poem in highlighted_poems_details:
+  titles.append(poem[0])
+  poets.append(poem[1])
+  dates.append(poem[2])
+  
+for poem in range(0, len(highlighted_poems_details)):
+  print("The poem {} was published by {} in {}".format(titles[poem], poets[poem], dates[poem]))
