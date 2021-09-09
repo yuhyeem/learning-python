3b86e9cf4014f29128dd1c0edb1adb96a8ce4fbc
Yeem <nayeem90325339@gmail.com>
2021-09-09 03:07:08 -0400

    used .find to find index of w.e u enter

--- stringspy3.py
+++ stringspy3.py
@@ -125,3 +125,9 @@ toomer_bio = \
 Nathan Pinchback Tomer, who adopted the name Jean Tomer early in his literary career, was born in Washington, D.C. in 1894. Jean is the son of Nathan Tomer was a mixed-race freedman, born into slavery in 1839 in Chatham County, North Carolina. Jean Tomer is most well known for his first book Cane, which vividly portrays the life of African-Americans in southern farmlands.
 """
 toomer_bio_fixed = toomer_bio.replace("Tomer","Toomer")
+
+#used .find to find index of w.e u enter
+god_wills_it_line_one = "The very earth will disown you"
+
+disown_placement = god_wills_it_line_one.find("disown")
+print (disown_placement)
