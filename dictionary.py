785d0686e60797090661bcddd9fd8f64b4a6f635
Yeem <nayeem90325339@gmail.com>
2021-09-12 01:18:17 -0400

    combining lists into a dictionary

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+
+#combining lists into dictionary
+
+drinks = ["espresso", "chai", "decaf", "drip"]
+caffeine = [64, 40, 0, 120]
+zipped_drinks = zip(drinks, caffeine)
+
+drinks_to_caffeine = {key: value for key,value in zipped_drinks}
+print(drinks_to_caffeine)
