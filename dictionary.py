3f128e5c311b0624b5b8457b0ec55919dd07ad98
Yeem <nayeem90325339@gmail.com>
2021-09-12 01:40:17 -0400

    added an exercise using the .get function on lists

--- dictionary.py
+++ dictionary.py
@@ -7,3 +7,12 @@ zipped_drinks = zip(drinks, caffeine)
 
 drinks_to_caffeine = {key: value for key,value in zipped_drinks}
 print(drinks_to_caffeine)
+
+# .get function practice
+
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+
+tc_id = user_ids.get("teraCoder", 100000)
+print(tc_id)
+
+stack_id = user_ids.get("superStackSmash", 100000)
